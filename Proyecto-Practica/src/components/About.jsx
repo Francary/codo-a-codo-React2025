2243db80ace17d6088a249fb10bf8e036fe90847
Francary <francarymolinapaez@gmail.com>
2025-10-25 14:09:56 -0300

    Clase06 - Mejora del CSS

--- Proyecto-Practica/src/components/About.jsx
+++ Proyecto-Practica/src/components/About.jsx
@@ -1,5 +1,16 @@
-const About = () =>{
-    return <h1>Proximamente Disponible mas info</h1>
+const About = () => {
+    return (
+        <>
+            <h1>Sobre nosotros</h1>
+            <p>
+                En TalentoLab, unimos moda y tecnología para crear una experiencia de compra pensada para quienes buscan estilo, innovación y calidad. Cada prenda y dispositivo que ofrecemos está cuidadosamente seleccionado para acompañarte en tu día a día con comodidad y personalidad.
+
+                Nuestro compromiso es brindarte productos de alta calidad, atención cercana y una compra ágil, segura y confiable. En TalentoLab, vestimos tu talento y conectamos con tu mundo.
+            </p>
+        </>
+
+    )
+
 }
 
 export {
